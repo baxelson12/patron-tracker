55315d57a7df83f3ec4ee6d0cb7078b450f97cd3
Brad <bradaxelson@gmail.com>
2020-11-02 11:27:11 -0500

    feat: generate frontend

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +13,7 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+  app.enableCors();
   await app.listen(port);
   console.log(`Server running on port ${port}`);
 }
