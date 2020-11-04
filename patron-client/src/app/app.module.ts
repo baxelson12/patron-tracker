47804d1b025b8cf7500dbfa1beeff80ed4f3e535
Brad <bradaxelson@gmail.com>
2020-11-04 00:11:20 -0500

    feat: PWA functionality for patron client

--- patron-client/src/app/app.module.ts
+++ patron-client/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { NgxMaskModule } from 'ngx-mask';
 import { QrCodeModule } from 'ng-qrcode';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,8 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     QrCodeModule,
     ReactiveFormsModule,
-    NgxMaskModule.forRoot()
+    NgxMaskModule.forRoot(),
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
