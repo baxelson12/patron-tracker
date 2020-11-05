4bc86fd9012b116441e9fc0920f4b94d8bed8f37
Brad <bradaxelson@gmail.com>
2020-11-05 11:02:19 -0500

    [api][fix] Static excludes not working, revert to separate urls

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,14 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Controller, Get, Redirect, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('patron-app')
-  sendPatronApplication(@Res() res) {
-    res.sendFile('index.html', {
-      root: 'dist/client/patron'
-    })
-  }
+  // @Get('patron-app')
+  // sendPatronApplication(@Res() res) {
+  //   res.sendFile('index.html', {
+  //     root: 'dist/client/patron'
+  //   })
+  // }
 }
