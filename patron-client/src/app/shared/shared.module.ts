5f1a8b8eda55c5af4cc8f7f7d74e54ee1c8e47cc
Brad <bradaxelson@gmail.com>
2020-11-06 15:28:13 -0500

    [patron][refactor] Build out card component

--- patron-client/src/app/shared/shared.module.ts
+++ patron-client/src/app/shared/shared.module.ts
@@ -5,18 +5,20 @@ import { CardInnerDirective } from './directives/card/card-inner.directive';
 import { CardImageDirective } from './directives/card/card-image.directive';
 import { CardContentDirective } from './directives/card/card-content.directive';
 import { CardInfoDirective } from './directives/card/card-info.directive';
+import { CardComponent } from './components/card/card.component';
 
 
 
 @NgModule({
-  declarations: [CardWrapperDirective, CardInnerDirective, CardImageDirective, CardContentDirective, CardInfoDirective],
+  declarations: [CardWrapperDirective, CardInnerDirective, CardImageDirective, CardContentDirective, CardInfoDirective, CardComponent],
   imports: [CommonModule],
   exports: [
     CardWrapperDirective,
     CardInnerDirective,
     CardImageDirective,
     CardContentDirective,
-    CardInfoDirective
+    CardInfoDirective,
+    CardComponent
   ]
 })
 export class SharedModule { }
