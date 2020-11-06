409fd123237f587dc1b6f1ec9a8802c098e2e244
Brad <bradaxelson@gmail.com>
2020-11-05 23:53:12 -0500

    [api][refactor] Should use exceptions

--- src/auth/strategies/local.strategy.ts
+++ src/auth/strategies/local.strategy.ts
@@ -2,6 +2,7 @@ import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import { Strategy } from "passport-local";
 import { AuthService } from "../auth.service";
+import { InvalidCredentials } from "../exceptions/user";
 
 @Injectable()
 export class EmployeeStrategy extends PassportStrategy(Strategy) {
@@ -11,7 +12,7 @@ export class EmployeeStrategy extends PassportStrategy(Strategy) {
 
     async validate(username: string, password: string): Promise<any> {
         const employee = await this.as.validate({username, password});
-        if (!employee) { throw new UnauthorizedException(); }
+        if (!employee) { throw InvalidCredentials }
 
         return employee;
     }
