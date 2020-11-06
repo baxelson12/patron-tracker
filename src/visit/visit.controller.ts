12824d0fb5a0b88568db15a13df6cd1f19179eac
Brad <bradaxelson@gmail.com>
2020-11-05 23:41:53 -0500

    [api][refactor] Remove unnecessary items

--- src/visit/visit.controller.ts
+++ src/visit/visit.controller.ts
@@ -1,13 +1,9 @@
 import { Controller, Get, Param, Post, UseGuards, Request, Body, Query } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
 import { JwtGuard } from 'src/auth/guards/jwt-auth.guard';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
-import { EmployeeService } from 'src/employee/employee.service';
 import { EmployeeRole } from 'src/shared/constants';
 import { Roles } from 'src/auth/decorators/roles.decorator';
 import { CreatePatronDto } from 'src/shared/dto/patron.dto';
-import { Visit } from 'src/shared/entities/visit.entity';
-import { Repository } from 'typeorm';
 import { VisitService } from './visit.service';
 
 @UseGuards(JwtGuard, RolesGuard)
@@ -25,8 +21,7 @@ export class VisitController {
 
     @Get()
     @Roles(EmployeeRole.OVERSEER, EmployeeRole.ADMIN)
-    getAll(@Request() req, @Query() query) {
-        const { start, end } = query;
+    getAll(@Request() req) {
         return this.vs.findAll(req.user.id);
     }
 
