052b246f9a329d9fd2b568ef37f8912430e233af
Brad <bradaxelson@gmail.com>
2020-11-06 23:13:12 -0500

    [client][fix] unauthorized routes not redirecting to login

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,6 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: 'login',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
     path: 'employee',
     loadChildren: () => import('./employee/employee.module').then(m=> m.EmployeeModule)
   },
