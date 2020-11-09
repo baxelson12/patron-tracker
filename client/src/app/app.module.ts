ff64632d4fef5c53a6fbb56072e5e2e7d6042332
Brad <bradaxelson@gmail.com>
2020-11-08 22:23:57 -0500

    [client][feat] Add in toasts

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { QrCodeModule } from 'ng-qrcode';
 import { NgxMaskModule } from 'ngx-mask';
@@ -7,6 +8,7 @@ import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptor } from './shared/interceptors/auth.interceptor';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -14,11 +16,17 @@ import { AuthInterceptor } from './shared/interceptors/auth.interceptor';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     QrCodeModule,
     ReactiveFormsModule,
     NgxMaskModule.forRoot(),
-    HttpClientModule
+    HttpClientModule,
+    ToastrModule.forRoot({
+      easing: 'ease-in',
+      easeTime: 200,
+      positionClass: 'toast-bottom-full-width'
+    })
   ],
   providers: [{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }],
   bootstrap: [AppComponent]
